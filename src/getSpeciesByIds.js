d700b1f12079ff4ebbda92c8455d73e1fbea309d
Fernando Bueno <fernando_jordi@hotmail.com>
2022-03-16 17:21:57 -0300

    adiciona requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  let animalSearch = [];
+  if (!ids) {
+    return animalSearch;
+  }
+  animalSearch = data.species.filter((speciesIdFilter) => {
+    const verification = ids.some((id) => speciesIdFilter.id === id);
+    return verification;
+  });
+  console.log(animalSearch);
+  return animalSearch;
 }
+
+getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce');
 
 module.exports = getSpeciesByIds;
