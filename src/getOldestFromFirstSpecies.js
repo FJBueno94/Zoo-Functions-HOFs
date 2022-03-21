edfd999186116df6ad9882dfe9455f840fb2bada
Fernando Bueno <fernando_jordi@hotmail.com>
2022-03-21 11:44:25 -0300

    adiciona o requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
+const { employees, species } = data;
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const findEmployee = employees.find((idFind) => idFind.id === id);
+  const { responsibleFor } = findEmployee;
+  const findSpecie = species.find((specieFind) => specieFind.id === responsibleFor[0]);
+  const findOlder = findSpecie.residents.sort((a, b) => b.age - a.age);
+  return Object.values(findOlder[0]);
 }
 
+console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 module.exports = getOldestFromFirstSpecies;
