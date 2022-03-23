ecbf59b4cff77ce23461e87557b0b31aa036c6e2
Fernando Bueno <fernando_jordi@hotmail.com>
2022-03-23 19:02:47 -0300

    adiciona o requisito 7

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,44 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
+const noParameter = () => {
+  const obj = {};
+  species.forEach((e) => {
+    if (!obj[e.location]) {
+      obj[e.location] = [];
+    }
+    obj[e.location].push(e.name);
+  });
+  return obj;
+};
+
+const includeName = (sex, sorted) => {
+  const obj = {};
+  species.forEach((e) => {
+    if (!obj[e.location]) {
+      obj[e.location] = [];
+    }
+    let residents1 = e.residents.map((e1) => e1.name);
+    if (sorted) residents1.sort();
+    if (sex) {
+      residents1 = [];
+      e.residents.filter((e2) => e2.sex === sex).map((e1) => {
+        residents1.push(e1.name);
+        return residents1;
+      });
+      if (sorted) residents1.sort();
+    }
+    obj[e.location].push({ [e.name]: residents1 });
+  });
+  return obj;
+};
+
 function getAnimalMap(options) {
-  // seu código aqui
+  if (!options) return noParameter();
+  const { includeNames, sex, sorted } = options;
+  if (includeNames === true) return includeName(sex, sorted);
+  return noParameter();
 }
 
 module.exports = getAnimalMap;
